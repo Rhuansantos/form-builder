14e154d2251551d18a5c4f233bf71550f818d348
Rhuan Souza <rhuansantosdev@gmail.com>
2017-10-24 00:24:15 -0400

    update node example to use my own package

--- usage-examples/node/src/controllers/root.js
+++ usage-examples/node/src/controllers/root.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { FormBuilder } = require('../../../../index');
+const { FormBuilder } = require('htmlform-builder');
+
 
 module.exports = function(app) {
   app.use('/', router);
